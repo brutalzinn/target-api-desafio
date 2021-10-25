e888c0d81c4fbc68c5f83ae8d0ea994bf132179e
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2021-10-25 17:29:05 -0300

    added scripts and postman update collection

---
+++ scripts/insert_vip.sql
@@ -0,0 +1,16 @@
+USE [targetapidb]
+GO
+
+INSERT INTO [dbo].[VipModel]
+           ([Id]
+           ,[Nome]
+           ,[Preco]
+           ,[Descricao])
+     VALUES
+           (1
+           ,'Vip 1'
+           ,50.00
+           ,'Você ganhou um super robô para ajudar nos seus investimentos. Dica: Invista na YuriCorp')
+GO
+
+
